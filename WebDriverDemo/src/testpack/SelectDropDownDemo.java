4251d64670c61e91df66688cabe9fed8a80a0465
NeelWebApp <neelamegam.rengasamy@gmail.com>
2020-02-22 09:47:38 +0530

    updated comments

--- WebDriverDemo/src/testpack/SelectDropDownDemo.java
+++ WebDriverDemo/src/testpack/SelectDropDownDemo.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SelectDropDownDemo {
-
+ //Testig
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Neel\\Desktop\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
